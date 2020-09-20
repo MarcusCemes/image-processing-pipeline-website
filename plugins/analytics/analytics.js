73e5397cc4ce6ca19a830a0c2f5ef73c7cf4e20d
Marcus Cemes <20288819+MarcusCemes@users.noreply.github.com>
2020-09-20 18:18:04 +0200

    fix: reported analytics path
    Removed prepended slash from the path.

--- plugins/analytics/analytics.js
+++ plugins/analytics/analytics.js
@@ -7,7 +7,7 @@ export default (function () {
 
   return {
     onRouteUpdate({ location }) {
-      _paq.push(["setCustomUrl", "/" + location.pathname]);
+      _paq.push(["setCustomUrl", location.pathname]);
       _paq.push(["setDocumentTitle", document.title]);
       _paq.push(["trackPageView"]);
     },
